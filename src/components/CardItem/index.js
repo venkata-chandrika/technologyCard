df2cb67f4dc23916f94e371acec81f49fa934565
chandrika <balimidivenkatachandrika@gmail.com>
2023-03-30 11:08:07 +0000

    tech cards

--- src/components/CardItem/index.js
+++ src/components/CardItem/index.js
@@ -6,7 +6,7 @@ const CardItem = props => {
   const {title, description, className, imgUrl} = cardsListDetails
   return (
     <li className={`${className} card`}>
-      <div>
+      <div className="eachCard">
         <h1 className="heading">{title}</h1>
         <p className="para">{description}</p>
         <img src={imgUrl} className="img" alt={title} />
